96927873b80da5c672da964451734d2bee8c4db2
Dharshini-2005 <dharshini.n2023csbs@sece.ac.in>
2026-09-09 23:46:14 +0530

    chore: update API base URL to deployed backend

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://tripease-backendd.onrender.com";
 export default BASE_URL;
